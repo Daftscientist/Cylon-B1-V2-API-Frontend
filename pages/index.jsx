a64490a828c2fb97ba45c2acc48d777aa41a6dbd
daftscientist <leojohnston12032@gmail.com>
2022-08-15 21:04:45 +0100

    dfs

--- pages/index.jsx
+++ pages/index.jsx
@@ -13,7 +13,7 @@ export default function Home() {
     <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
       <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
         <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-          <Image className="inline" src={CylonLogo} width={"40px"} height={"40px"} alt="Cylon Logo" />Create an account
+          <Image className="inline" src={CylonLogo} width={"30px"} height={"30px"} alt="Cylon Logo" /><div className="pl-2 inline">Create an account</div>
         </h1>
         <form className="space-y-4 md:space-y-6" action="#">
           <div>
@@ -48,22 +48,6 @@ export default function Home() {
               required=""
             />
           </div>
-          <div>
-            <label
-              htmlFor="confirm-password"
-              className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
-            >
-              Confirm password
-            </label>
-            <input
-              type="confirm-password"
-              name="confirm-password"
-              id="confirm-password"
-              placeholder="••••••••"
-              className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-              required=""
-            />
-          </div>
           <div className="flex items-start">
             <div className="flex items-center h-5">
               <input
@@ -79,7 +63,7 @@ export default function Home() {
                 htmlFor="terms"
                 className="font-light text-gray-500 dark:text-gray-300"
               >
-                I accept the{" "}
+                I accept the{' '}
                 <a
                   className="font-medium text-primary-600 hover:underline dark:text-primary-500"
                   href="#"
