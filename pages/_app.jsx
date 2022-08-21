efc2de09dc6cc5c6bae796be8d25823c340204ec
daftscientist <leojohnston12032@gmail.com>
2022-08-21 15:13:48 +0100

    Added top loading indicator instead of old one.

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,6 +1,13 @@
 import '../styles/globals.css'
 import { Toaster } from 'react-hot-toast';
+import Router from 'next/router';
 import Navigation from '../components/navigation/navigation';
+import NProgress from 'nprogress';
+import 'nprogress/nprogress.css';
+
+Router.events.on('routeChangeStart', () => NProgress.start()); 
+Router.events.on('routeChangeComplete', () => NProgress.done()); 
+Router.events.on('routeChangeError', () => NProgress.done());
 
 function App({ Component, pageProps }) {
   return (
